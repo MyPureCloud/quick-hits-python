ee20581de4daaea57729c4c85c9a763d765262ff
Tim Smith <74417894+timsmithgenesys@users.noreply.github.com>
2020-11-23 14:14:31 -0700

    Create overview.py

---
+++ getting-started/sdk-basics/overview.py
@@ -0,0 +1,10 @@
+// >> START sdk-overview This example demonstrates authorizing using a client credentials grant and getting authorization permissions
+import PureCloudPlatformClientV2
+
+# Creates an api client
+apiclient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token("a0bda580-cb41-4ff6-8f06-28ffb4227594", "e4meQ53cXGq53j6uffdULVjRl8It8M3FVsupKei0nSg")
+
+# Create Auth API Instance
+authApi = PureCloudPlatformClientV2.AuthorizationApi(apiclient)
+print authApi.get_authorization_permissions()
+// >> END sdk-overview
