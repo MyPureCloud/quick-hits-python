ba9f4d3bc6f6f754d10b0d70a9abc23b17960ef2
Prince Merluza <zero.bash@gmail.com>
2021-12-09 15:01:16 +0800

    DEVENGAGE-886 Add oauth-client-credentials

---
+++ authorization/oauth-client-credentials/oauth-client-credentials.py
@@ -0,0 +1,61 @@
+# >> START oauth-client-credentials Client credentials grant login without a client library. 
+
+import base64, requests, sys, os
+
+print("---------------------------------------------------")
+print("- Genesys Cloud Python Client Credentials Example -")
+print("---------------------------------------------------")
+
+CLIENT_ID = os.environ['GENESYS_CLOUD_CLIENT_ID']
+CLIENT_SECRET = os.environ['GENESYS_CLOUD_CLIENT_SECRET']
+ENVIRONMENT = os.environ['GENESYS_CLOUD_ENVIRONMENT'] # eg. mypurecloud.com
+
+
+# Base64 encode the client ID and client secret
+authorization = base64.b64encode(bytes(CLIENT_ID + ":" + CLIENT_SECRET, "ISO-8859-1")).decode("ascii")
+
+# Prepare for POST /oauth/token request
+request_headers = {
+    "Authorization": f"Basic {authorization}",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+request_body = {
+    "grant_type": "client_credentials"
+}
+
+# Get token
+response = requests.post(f"https://login.{ENVIRONMENT}/oauth/token", data=request_body, headers=request_headers)
+
+# Check response
+if response.status_code == 200:
+    print("Got token")
+else:
+    print(f"Failure: { str(response.status_code) } - { response.reason }")
+    sys.exit(response.status_code)
+
+# Get JSON response body
+response_json = response.json()
+
+# Prepare for GET /api/v2/authorization/roles request
+requestHeaders = {
+    "Authorization": f"{ response_json['token_type'] } { response_json['access_token']}"
+}
+
+# Get roles
+response = requests.get(f"https://api.{ENVIRONMENT}/api/v2/authorization/roles", headers=requestHeaders)
+
+# Check response
+if response.status_code == 200:
+    print("Got roles")
+else:
+    print(f"Failure: { str(response.status_code) } - { response.reason }")
+    sys.exit(response.status_code)
+
+# Print roles
+print("\nRoles:")
+for entity in response.json()["entities"]:
+    print(f"  { entity['name'] }")
+
+print("\nDone")
+
+# >> END
